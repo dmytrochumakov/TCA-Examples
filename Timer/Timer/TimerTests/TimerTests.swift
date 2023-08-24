066778fe52b986a4df69e14709775e3be3bf4e52
Dmytro Chumakov <dmytro.chumakov@devlight.io>
2023-08-24 10:05:56 +0300

    added: - testTogglginTimer

--- Timer/Timer/TimerTests/TimerTests.swift
+++ Timer/Timer/TimerTests/TimerTests.swift
@@ -5,31 +5,24 @@
 //  Created by Dmytro Chumakov on 17.08.2023.
 //
 
+import ComposableArchitecture
 import XCTest
 @testable import Timer
 
+@MainActor
 final class TimerTests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
+    func testTogglginTimer() async throws {
+        let store = TestStore(initialState: TimerFeature.State()) {
+            TimerFeature()
+        }
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
+        await store.send(.startTapped) {
+            $0.isTimerActive = true
+        }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+        await store.send(.stopTapped) {
+            $0.isTimerActive = false
         }
     }
 
